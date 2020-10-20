021782061cab816d1a5c421d91bbdc367eb1577e
João Dartora <joao.dartora@ilegra.com>
2020-10-19 23:09:53 -0300

    feat: improving examples

--- generics/src/com/joaodartora/generics/multipletypeparameters/HashMapDemo.java
+++ generics/src/com/joaodartora/generics/multipletypeparameters/HashMapDemo.java
@@ -1,5 +1,9 @@
 package com.joaodartora.generics.multipletypeparameters;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
 
 //    You can pass as many type parameters as you want to a class
@@ -13,6 +17,10 @@ public class HashMapDemo {
 
         System.out.println("Dry: " + bin.getDryTrash() + " Wet: " + bin.getWetTrash());
 
+        Map<String, BigDecimal> codeValueMap = new HashMap<>();
+        codeValueMap.put("Bankslip", BigDecimal.TEN);
+
+        System.out.println("Bankslip value: " + codeValueMap.get("Bankslip"));
     }
 
 }
